3fb0df57c3597ba073db295cd4aa0eda855359bd
Your Name <you@example.com>
2024-02-04 21:20:32 +0100

    aggiunte migrate e seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             PostSeeder::class,
+            TagSeeder::class,
+
         ]);
     }
 }
--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tag;
+
 
 class TagSeeder extends Seeder
 {
@@ -12,6 +14,25 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tags = [
+            [
+                'name' => 'cronaca',
+                'description' => 'articoli che riguardano fatti di cronaca',
+            ],
+            [
+                'name' => 'tecnologia',
+                'description' => 'troverai le più recenti novità in ambito hi-tech',
+            ],
+            [
+                'name' => 'sport',
+                'description' => 'non guardo il calcio',
+            ]
+        ];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->fill($tag);
+            $newTag->save();
+        }
     }
 }
